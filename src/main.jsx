8afda9931087041a658f13d6b4a30110c418cc01
Durjoy96 <mddurjoy853@gmail.com>
2024-10-30 17:32:40 +0600

    creating contact and url params as a loader

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./Root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/Root";
 import ErrorPage from "./ErrorPage";
-import Contact from "./routes/Contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
+    action: rootAction,
+    loader: rootLoader,
     children: [
       {
         path: "/contacts/:contactId",
+        loader: contactLoader,
         element: <Contact></Contact>,
       },
     ],
